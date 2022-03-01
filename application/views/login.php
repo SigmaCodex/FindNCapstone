9a1bf597056ac8bcccb62274c5626e48c089cad7
cdtanggol@gmail.com <cdtanggol@gmail.com>
2022-03-01 16:26:25 +0800

    Edit: Login Page Colours

---
+++ application/views/login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to FindN</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/loginpage.css">
+	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<img class="wave" src="assets/images/wavy.png">
+	<div class="container">
+		<div class="img">
+			<img src="assets/images/findn.png">
+		</div>
+		<div class="login-content">
+			<form action="index.html">
+				<img src="assets/images/login-image.png">
+				<h2 class="title">Welcome</h2>
+           		<div class="input-div one">
+           		   <div class="i">
+           		   		<i class="fas fa-user"></i>
+           		   </div>
+           		   <div class="div">
+           		   		<h5>Username</h5>
+           		   		<input type="text" class="input">
+           		   </div>
+           		</div>
+           		<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<h5>Password</h5>
+           		    	<input type="password" class="input">
+            	   </div>
+            	</div>
+            	<a href="#">Forgot Password?</a>
+            	<input type="submit" class="btn" value="Login">
+            </form>
+        </div>
+    </div>
+    <script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
