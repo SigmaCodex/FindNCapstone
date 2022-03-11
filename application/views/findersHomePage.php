99cf311ba37dcaa3914f3841d1e65a5ce303ec96
JunkyDubs <junkydublin@gmail.com>
2022-03-11 23:37:40 +0700

    Finders_HomePage

---
+++ application/views/findersHomePage.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Select Shop</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="assets/css/findershomepage.css">
+
+</head>
+<body>
+
+    <!--Navigation Bar-->
+    <input type="checkbox" id="check">
+<!--Start header-->
+<header class="p-2">
+    <label for="check">
+        <i class="fa-solid fa-bars" id="side_bar_btn" onclick = "btn();"></i>
+    </label>
+    <div class="logo_holder d-flex justify-content-center align-items-center align-self-center" >
+        <img class="logo" src="assets/images/logo.png"> 
+        <p class="fs-4 pt-2 m-0 fw-bolder">FindN</p>
+    </div>
+   
+</header>
+<!--End header-->
+
+<!--Sidebar start-->
+
+<div class="side_bar" id="side_bar_show" style="display:none;" >
+    <div class="centerA">
+        <img src="assets/images/Prof.png" class="profile_image" alt="">
+        <h4>Junky Dubs</h4>
+    </div>
+    <a href="#" class="m-0 w-100"><i class="fa-solid fa-home fa-fw"></i><span>Home</span></a>
+    <a href="#" class="m-0 w-100"><i class="fa-solid fa-history"></i><span>History</span></a>
+    <a href="#" class="m-0 w-100"><i class="fa-solid fa-tasks"></i><span>Pending Booking</span></a>
+    <a href="#" class="m-0 w-100"><i class="fa-solid fa-share-alt"></i><span>Settings</span></a>
+    <a href="#" class="m-0 w-100"><i class="fa-solid fa-question-circle"></i><span>About</span></a>
+    <a href="#" class="m-0 w-100"><i class="fa-solid fa-door-open"></i><span>Logout</span></a>
+    
+</div>
+<!--Content Inside-->
+<div class="container ">
+    <div class="row content-holder mt-5">
+        <!--Left side column-->
+        <div class="col-md-9 col-sm-9 col-xs-9"> 
+            <div class="row text-center">
+                <div class="col-md-4 col-sm-6 my-3 my-md-0 d-flex justify-content-around align-items-center">
+                        <!-- TNC Computer Shop-->
+                <form action="index.php" method="post">
+                            <div class="card shadow">
+                                <div>
+                                    <img src="assets/images/Image1.png " alt="Image1" class="img-fluid card-img-top">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">TNC Cebu HQ</h5>
+                                    <h6>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                    </h6>
+                                    <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i> II Pelaez St. Brgy. Kalubihan Cebu City, 6000
+                                    </p>
+                                    <p>
+                                    <i class="fa-solid fa-clock"></i> Open <strong>24/7</strong>
+                                    </p>
+                                    <h6>
+                                        <span class="price">Regular Hours - ₱15</span>
+                                    </h6>
+                                    <h6>
+                                        <span class="price">VIP Hours - <small><s class="text-secondary">₱25</s></small> ₱20 </span>
+                                    </h6>
+                                    <button type="button" class="btn btn-success my-3" name="view">View Shop <i class="fa-solid fa-eye"></i></button>
+                                    </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4 col-sm-6 my-3 my-md-0 py-3">
+                        <!-- GameCity Computer Shop-->
+                <form action="index.php" method="post">
+                            <div class="card shadow">
+                                <div>
+                                    <img src="assets/images/Image2.png " alt="Image2" class="img-fluid card-img-top">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Game City CyberCafe</h5>
+                                    <h6>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                    </h6>
+                                    <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i> Arcenas, Colon St, Cebu City, 6000 Cebu
+                                    </p>
+                                    <p>
+                                    <i class="fa-solid fa-clock"></i> Open <strong>Monday-Sunday</strong>
+                                    </p>
+                                    <h6>
+                                        <span class="price">Regular Hours - ₱15</span>
+                                    </h6>
+                                    <h6>
+                                        <span class="price">VIP Hours - <small><s class="text-secondary">₱20</s></small> ₱18 </span>
+                                    </h6>
+                                    <button type="button" class="btn btn-success my-3" name="view">View Shop <i class="fa-solid fa-eye"></i></button>
+                                    </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4 col-sm-6 my-3 my-md-0 py-3">
+                        <!-- Voyager Computer Shop-->
+                <form action="index.php" method="post">
+                            <div class="card shadow">
+                                <div>
+                                    <img src="assets/images/Image3.jpg " alt="Image3" class="img-fluid card-img-top">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Voyager CyberCafe</h5>
+                                    <h6>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                    </h6>
+                                    <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i> Sanciangko St, Cebu City, Cebu 6000
+                                    </p>
+                                    <p>
+                                    <i class="fa-solid fa-clock"></i> Open <strong>24/7</strong>
+                                    </p>
+                                    <h6>
+                                        <span class="price">Regular Hours - ₱20</span>
+                                    </h6>
+                                    <h6>
+                                        <span class="price">(5) hours or more- <small><s class="text-secondary">₱20</s></small> ₱15 </span>
+                                    </h6>
+                                    <button type="button" class="btn btn-success my-3" name="view">View Shop <i class="fa-solid fa-eye"></i></button>
+                                    </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4 col-sm-6 my-3 my-md-0 py-3">
+                        <!-- G7 Computer Shop-->
+                <form action="index.php" method="post">
+                            <div class="card shadow">
+                                <div>
+                                    <img src="assets/images/Image4.png " alt="Image4" class="img-fluid card-img-top">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">G-7 CyberCafe</h5>
+                                    <h6>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                    </h6>
+                                    <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i> Dreamfield Building, Sanciangko St, Cebu City
+                                    </p>
+                                    <p>
+                                    <i class="fa-solid fa-clock"></i> Open <strong>24/7</strong>
+                                    </p>
+                                    <h6>
+                                        <span class="price">Regular Hour - ₱15</span>
+                                    </h6>
+                                    <h6>
+                                        <span class="price">VIP Hour - <small><s class="text-secondary">₱25</s></small> ₱20 </span>
+                                    </h6>
+                                    <button type="button" class="btn btn-success my-3" name="view">View Shop <i class="fa-solid fa-eye"></i></button>
+                                    </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4 col-sm-6 my-3 my-md-0 py-3">
+                        <!-- Level Cap Gaming Hub-->
+                <form action="index.php" method="post">
+                            <div class="card shadow">
+                                <div>
+                                    <img src="assets/images/Image5.jpg " alt="Image5" class="img-fluid card-img-top">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Level Cap Gaming Hub</h5>
+                                    <h6>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                    </h6>
+                                    <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i> Country Drive, Lungsod ng Cebu, Lalawigan ng Cebu
+                                    </p>
+                                    <p>
+                                    <i class="fa-solid fa-clock"></i> Open <strong>24/7</strong>
+                                    </p>
+                                    <h6>
+                                        <span class="price">Regular Hour - ₱15</span>
+                                    </h6>
+                                    <h6>
+                                        <span class="price">VIP Hour - <small><s class="text-secondary">₱25</s></small> ₱20 </span>
+                                    </h6>
+                                    <button type="button" class="btn btn-success my-3" name="view">View Shop <i class="fa-solid fa-eye"></i></button>
+                                    </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-md-4 col-sm-6 my-3 my-md-0 py-3">
+                        <!-- G-Side Net Cafe-->
+                <form action="index.php" method="post"> 
+                            <div class="card shadow">
+                                <div>
+                                    <img src="assets/images/Image6.png " alt="Image6" class="img-fluid card-img-top">
+                                </div>
+                                <div class="card-body">
+                                    <h5 class="card-title">G-Side Net Cafe</h5>
+                                    <h6>
+                                        <i class="fas fa-star"></i>
+                                        <i class="fas fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                        <i class="far fa-star"></i>
+                                    </h6>
+                                    <p class="card-text">
+                                    <i class="fa-solid fa-location-dot"></i> 679 Bayong - Yati Rd, Liloan, Cebu
+                                    </p>
+                                    <p>
+                                    <i class="fa-solid fa-clock"></i> Open <strong>24/7</strong>
+                                    </p>
+                                    <h6>
+                                        <span class="price">Regular Hour - ₱12</span>
+                                    </h6>
+                                    <h6>
+                                        <span class="price"> <small><s class="text-secondary">‎</s></small>‎ </span>
+                                    </h6>
+                                    <button type="button" class="btn btn-success my-3" name="view">View Shop <i class="fa-solid fa-eye"></i></button>
+                                    </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        <!--Right side column-->
+        <div class="col-md-3 col-sm-3 col-xs-3 my-3 my-md-0">
+            <div class="right-sidebar card shadow"> 
+                <div class="sidebar-title">
+                        <h4>Events</h4>
+                        <a href="#">See All</a>
+                    </div>
+
+                    <div class="event">
+                        <div class="left-event">
+                            <h2>3</h2>
+                            <span>March</span>
+                        </div>
+                        <div class="right-event">
+                            <h4>FindN Meeting</h4>
+                            <p><i class="fa-solid fa-magnifying-glass-location"></i>FindN Company</p>
+                            <a href="https://www.facebook.com/findN.cebu.ph">More Info</a>
+                        </div>
+                    </div>
+                    <div class="event">
+                        <div class="left-event">
+                            <h2>5</h2>
+                            <span>March</span>
+                        </div>
+                        <div class="right-event">
+                            <h4>Alliance Seminar</h4>
+                            <p><i class="fa-brands fa-accusoft"></i>Alliance Corporation</p>
+                            <a href="https://www.allianceglobalinc.com/">More Info</a>
+                        </div>
+                    </div>
+                    <div class="sidebar-title">
+                        <h4>Advertisement</h4>
+                            <a href="#">Close</a>
+                    </div>
+                        <img src="assets/images/advertisement1.png" class="sidebar-ads">
+                        <img src="assets/images/ads2.png" class="sidebar-ads">
+                        <img src="assets/images/ads3.jpeg" class="sidebar-ads">
+                </div>
+            </div>
+    </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+</body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        function btn(){
+            let side_bar = document.getElementById("side_bar_show")
+
+            if (side_bar.style.display === "none") {
+                side_bar.style.display = "block"
+            } else {
+                side_bar.style.display = "none"
+            }
+        }
+    </script>
+</html>
