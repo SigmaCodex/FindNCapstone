c334d5c08c222ad524a7b5808a488a061321416d
zendaz <Naviodlases@gmail.com>
2022-03-12 23:06:33 +0800

    back end finished

--- application/models/MainModel.php
+++ application/models/MainModel.php
@@ -21,23 +21,48 @@ class MainModel extends CI_Model{
 
     //finders
     public function registerFinder(){
+        $Primarycode = 0;
         $Primarycode = $this->generatePrimarykey();
         //check if the primary key is taken;
         $this->db->where('user_id',$Primarycode);
         $query = $this->db->get('user');
         if(!empty($query->result_array()))
 		{ 
-            echo "duplicate primarycode";
+            $this->registerFind();
 		}
 		else
 		{
-            echo"wlay sulod Pwedi naka mo add sa imoha mga data diri";
+            $datauser = array(
+                'user_id'   =>    $Primarycode,
+                'username'  => 	  $this->input->post('username'),
+                'password'  =>    $this->input->post('conpass'),
+                'user_type' =>    "finder",
+                'status'    =>    "active"
+            );
+                $this->db->insert('user',$datauser);
+
+            // $this->db->select('user_id');
+            // $this->db->where('user_id',$Primarycode);
+            // $query2 = $this->db->get('user');
+            // $resultquery = $query2->row_array();
+            $datafinder = array(
+                'user_id'    =>      $Primarycode,
+                'lastname'   => 	 $this->input->post('lastname'),
+                'firstname'  =>      $this->input->post('firstname'),
+                'birthdate'  => 	 $this->input->post('birthdate'),
+                'gender'     => 	 $this->input->post('gender'),
+                'email'      =>      $this->input->post('email'),
+                'phone_num'  =>      $this->input->post('pnum'),
+                'vac_status' =>      $this->input->post('vacstatus')
+            );
+                $this->db->insert('finders',$datafinder);
+
 		}
     }
     public function generatePrimarykey(){
-        $first = date('ymd');
+        $first = date('dy');
         $second = rand (1, 100);
-        $third = rand (1, 200);
+        $third = rand (1, 10);
  
         return $first."".$second."".$third;
      }
