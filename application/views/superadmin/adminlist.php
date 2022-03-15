2a9a7f75dd8366172f401fcd2ca5988ffa18b9f7
zendaz <Naviodlases@gmail.com>
2022-03-15 12:29:47 +0800

    admin list update

--- application/views/superadmin/adminlist.php
+++ application/views/superadmin/adminlist.php
@@ -11,6 +11,12 @@
 	
 	<link rel="stylesheet" href="assets/adminlist/css/style.css">
 
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script> 
+
+  <link rel="stylesheet" href="assets/adminlist/css/ionicons.min.css">
+
 	</head>
 	<body>
 	<section class="ftco-section">
@@ -20,7 +26,7 @@
 					<h2 class="heading-section">Computer shop admin list</h2>
 				</div>
         <div class="col-md-3 text-right mb-4">
-        <button type="button" class="btn mb-2 mb-md-0 btn-primary btn-block">Add Admin</button>
+        <button type="button" class="btn mb-2 mb-md-0 btn-primary btn-block" data-target="#exampleModalCenter" data-toggle="modal">Add Admin</button>
 				</div>
 			</div>
       <br>
@@ -45,7 +51,6 @@
           </div>
         </div>
       </div>
-
 				<div class="col-md-9 col-xs-1">
 					<h3 class="h5 mb-4 text-center"></h3>
 					<div class="table-wrap">
@@ -53,10 +58,9 @@
 						  <thead class="thead-primary">
 						    <tr>
 						    	<th>&nbsp;</th>
-						    	<th>&nbsp;</th>
-						    	<th>Product</th>
-						      <th>Price</th>
-						      <th>Quantity</th>
+						    	<th>No.</th>
+						    	<th>Name</th>
+						      <th>Status</th>
 						      <th>Action</th>
 						    </tr>
 						  </thead>
@@ -64,7 +68,7 @@
 						    <tr class="alert" role="alert">
 						    	<td>
 						    		<label class="checkbox-wrap checkbox-primary">
-										  <input type="checkbox" checked>
+										  <input type="checkbox">
 										  <span class="checkmark"></span>
 										</label>
 						    	</td>
@@ -74,19 +78,15 @@
 						      <td>
 						      	<div class="email">
 						      		<span>Sneakers Shoes 2020 For Men </span>
-						      		<span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
 						      	</div>
 						      </td>
-						      <td>$44.99</td>
-				          </td>
-				          <td>$89.98</td>
+						      <td>Inactive</td>
 						      <td>
 						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
 				          	</button>
 				        	</td>
 						    </tr>
-
 						    <tr class="alert" role="alert">
 						    	<td>
 						    		<label class="checkbox-wrap checkbox-primary">
@@ -100,11 +100,9 @@
 						      <td>
 						      	<div class="email">
 						      		<span>Sneakers Shoes 2020 For Men </span>
-						      		<span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
 						      	</div>
 						      </td>
-						      <td>$30.99</td>
-				          <td>$30.99</td>
+						      <td>Inactive</td>
 						      <td>
 						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
@@ -125,11 +123,9 @@
 						      <td>
 						      	<div class="email">
 						      		<span>Sneakers Shoes 2020 For Men </span>
-						      		<span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
 						      	</div>
 						      </td>
-						      <td>$35.50</td>
-				          <td>$35.50</td>
+						      <td>Inactive</td>
 						      <td>
 						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
@@ -150,12 +146,9 @@
 						      <td>
 						      	<div class="email">
 						      		<span>Sneakers Shoes 2020 For Men </span>
-						      		<span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
 						      	</div>
 						      </td>
-						      <td>$76.99</td>
-
-				          <td>$76.99</td>
+						      <td>Inactive</td>
 						      <td>
 						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
@@ -176,11 +169,9 @@
 						      <td class="border-bottom-0">
 						      	<div class="email">
 						      		<span>Sneakers Shoes 2020 For Men </span>
-						      		<span>Fugiat voluptates quasi nemo, ipsa perferendis</span>
 						      	</div>
 						      </td>
-						      <td class="border-bottom-0">$40.00</td>
-				          <td class="border-bottom-0">$40.00</td>
+						      <td class="border-bottom-0">Inactive</td>
 						      <td class="border-bottom-0">
 						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
 				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
@@ -196,11 +187,117 @@
 		</div>
 	</section>
 
+  <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		  <div class="modal-dialog modal-dialog-centered" role="document">
+		    <div class="modal-content">
+		      <!-- <div class="modal-header">
+		        <button type="button" class="close d-flex align-items-center justify-content-center" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true" class="ion-ios-close"></span>
+		        </button>
+		      </div> -->
+		      <div class="modal-body p-4 py-5 p-md-5">
+		      	<h3 class="text-center mb-3">Add admin</h3>
+		      	<form action="#" class="signup-form">
+		      		<div class="form-group mb-2">
+		      			<label for="name">Username</label>
+		      			<input type="text" class="form-control">
+		      		</div>
+					  <div class="form-group mb-2">
+		      			<label for="name">Last Name</label>
+		      			<input type="text" class="form-control">
+		      		</div>
+					<div class="form-group mb-2">
+		      			<label for="name">First Name</label>
+		      			<input type="text" class="form-control">
+		      		</div>
+					  <div class="form-group mb-2">
+		      			<label for="name">Email</label>
+		      			<input type="email" class="form-control">
+		      		</div>
+					  <div class="form-group mb-2">
+		      			<label for="name">Phone number</label>
+		      			<input type="number" class="form-control">
+		      		</div>
+					  <div class="form-group mb-2">
+		      			<label for="name">Birthdate</label>
+		      			<input type="date" class="form-control">
+		      		</div>
+				<div class="form-group mb-2">
+                <label for="user_type">Vacination Status</label>
+	              <select class="form-control mb-2 form-select-lg mb-3 form-select" aria-label="Default select example">
+                  <option selected>1st Dose</option>
+                  <option option value="1">2nd Dose</option>
+                  </select>
+              	</div>	  
+				<div class="form-group mb-2">
+                <label for="user_type">Gender</label>
+	              <select class="form-control mb-2 form-select-lg mb-3 form-select" aria-label="Default select example">
+                  <option selected>Male</option>
+                  <option option value="1">Female</option>
+                  </select>
+              	</div>
+				  <div class="form-group mb-2">
+                <label for="user_type">Civil Status</label>
+	              <select class="form-control mb-2 form-select-lg mb-3 form-select" aria-label="Default select example">
+                  <option selected>Single</option>
+                  <option option value="1">Married</option>
+                  </select>
+              	</div>
+		    	<div class="form-group mb-2">
+                <label for="user_type">User type</label>
+	              <select class="form-control mb-2 form-select-lg mb-3 form-select" aria-label="Default select example">
+                  <option selected>Finder</option>
+                  <option option value="1">Admin</option>
+                  </select>
+              	</div>
+              <div class="form-group mb-2">
+                <label for="status">Status</label>
+	              <select class="form-control mb-2 form-select-lg mb-3 form-select" aria-label="Default select example">
+                  <option selected>Inactive</option>
+                  <option option value="1">Active</option>
+                </select>
+              </div>
+			  <div class="form-group mb-2">
+		      		<label for="name">Password</label>
+		      		<input type="password" class="form-control">
+		      	</div>
+				  <div class="form-group mb-2">
+		      		<label for="name">Repeat Password</label>
+		      		<input type="password" class="form-control">
+		      	</div>
+              <div class="modal-footer">
+                <div class="form-group mb-2">
+	            	  <button type="submit" class="close-btn form-control btn btn-primary rounded px-3">Cancel</button>
+	              </div>
+                <div class="form-group mb-2">
+	            	  <button type="submit" class="form-control btn btn-primary rounded submit px-3">Add</button>
+	              </div>
+              </div>
+	            
+	            <!-- <div class="form-group d-md-flex">
+								<div class="w-100 text-center">
+									<a href="#" class="forgot">I'm already a member</a>
+								</div>
+	            </div> -->
+	          </form>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
 	<script src="assets/adminlist/js/jquery.min.js"></script>
   <script src="assets/adminlist/js/popper.js"></script>
   <script src="assets/adminlist/js/bootstrap.min.js"></script>
   <script src="assets/adminlist/js/main.js"></script>
 
+  <script>
+    $(document).ready(function(){
+        // Close modal on button click
+        $(".close-btn").click(function(){
+            $("#exampleModalCenter").modal('hide');
+        });
+    });
+  </script>
 	</body>
 </html>
 
