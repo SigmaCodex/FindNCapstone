7457d64bcfbbc9d457ef31dd9aeeb934426aeff9
JunkyDubs <junkydublin@gmail.com>
2022-03-29 02:57:45 +0700

    Navbar Integrated to profile page and others

--- application/views/accountSettings.php
+++ application/views/accountSettings.php
@@ -4,98 +4,43 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-    <title>Document</title>
+    <title>Account Profile</title>
 
     <link rel="stylesheet" href="assets/css/accountSettings.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-    <!-- Navbar CSS & icons -->
-    <link rel="stylesheet" href="assets/css/navbar.css">
-    
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">    
 </head>
 <body>
     
-    <!-- HEADER -->
-    <header class="header" id="header ">
-        <nav class="nav container-nav ">
-            <img src="assets/images/icon-logo.png" class="nav__logo" style="width: 100px; padding-left: 10px;">
-
-            <div class="nav__menu" id="nav-menu ">
-                <ul class="nav__list">
-                    <!-- Home -->
-                    <li class="nav__item">
-                        <a href="#home" class="nav__link active-link">
-                            <i class='bx bx-home-alt nav__icon'></i>
-                            <span class="nav__name">Home</span>
-                        </a>
-                    </li>
-                    <!-- Search -->
-                    <li class="nav__item">
-                        <a href="#search" class="nav__link">
-                            <i class='bx bx-search-alt nav__icon'></i>
-                            <span class="nav__name">Search</span>
-                        </a>
-                    </li>
-                    <!-- Bookings -->
-                    <li class="nav__item">
-                        <a href="#bookings" class="nav__link">
-                            <i class='bx bx-book-alt nav__icon'></i>
-                            <span class="nav__name">Bookings</span>
-                        </a>
-                    </li>
-                    <!-- Notification -->
-                    <li class="nav__item">
-                        <a href="#notifications" class="nav__link">
-                            <i class='bx bx-bell nav__icon'></i>
-                            <span class="nav__name">Notifications</span>
-                        </a>
-                    </li>
-
-                </ul>
-            </div>
-
-            <!-- <img src="assets/images/perfil.png" alt="" class="nav__img"> -->
-            <a href="accountSettings"><img src="assets/images/Prof.png" class="nav__img"> Dnko12</a>
-        </nav>
-    </header>
-
-
-
 
 <!-- Content Inside -->
 
 <form method="post" id="upload_form" enctype="multipart/form-data"> 
-<div class="container my-2 ">
-    <div class="wrapper bg-white mt-sm-5">
+<div class="container">
+    <div class="wrapper bg-white">
         <h4 class="tit pb-4 border-bottom">Account Settings</h4>
         <div class="d-flex align-items-start "> 
 
-        <!-- <div class="pp pl-sm-4 pl-2" id="img-section"> <b>Profile Photo</b>
-                <p>Accepted file type (.png). Less than 5MB</p> <button class="btn button border"><b>Upload</b></button>
-            </div>  -->
-       <!-- img Upload Update -->
-            <!-- <img src="assets/Images/Prof.png" class="img" alt=""> -->
+        <div class="pp title-info" id="img-section" >Profile Photo
+                <p>Accepted file type (.png). Less than 5MB</p> 
+            </div> 
         </div>
         <?php foreach ($findersPersonalDetails as $w) {?> 
         <div class="py-2"> 
-            <div class="row py-2">
-                <div class="avatar-upload">
+            <div class="d-flex">
+                <div class="avatar-upload col-md-6 pt-md-0 pt-3">
                     <div class="avatar-edit">
                         <input type='file' id="imageUpload" name="imageUpload" accept=".png, .jpg, .jpeg" />
                         <label for="imageUpload"></label>
                     </div>
-                
                     <div class="avatar-preview">
-                            <div id="imagePreview" style="background-image: url(assets/upload/finder/<?php echo $w->profile_pic;?>);"></div>
+                        <div id="imagePreview" style="background-image: url(assets/upload/finder/<?php echo $w->profile_pic;?>);"></div>
                     </div>
                 </div>  
             </div>    
         </div>       
-            <div class="title-info"> <b>USER INFORMATION</b>
+            <div class="title-info"> USER INFORMATION
             </div>
             <div class="row py-2">
                 <div class="col-md-6"> <label for="firstname">First Name</label> <input type="text" name="f_name" value="<?php echo $w->firstname;?>" class="bg-light form-control"> </div>
@@ -109,13 +54,13 @@
                 </div>
             </div>
             <br>
-            <div class="title-info"> <b>ACCOUNT INFORMATION</b>
+            <div class="title-info"> ACCOUNT INFORMATION
              <div class="row py-2">
                 <div class="col-md-6"> <label for="username">Username</label> <input name="username" value="<?php if(isset($username)){ echo $username;}?>" type="text" class="bg-light form-control" disabled>  </div>
                 <div class="col-md-6 pt-md-0 pt-3"> <label for="email">Email</label> <input name="email" type="email" value="<?php echo $w->email;?>" class="bg-light form-control">  </div>
                 <div class="col-md-6"> <label for="phone">Phone Number</label> <input  name="p_number" value="<?php echo $w->phone_num;?>" type="text" class="bg-light form-control" placeholder="+63">  </div>
             </div>
-            <div class="title-info"> <b>VACCINATION STATUS</b></div>
+            <div class="title-info"> VACCINATION STATUS</div>
             <div class="row py-2">
                 <div class="col-md-6"> <label for="vaccine">Vaccination Form Status</label> 
                     <select id="vaccine" name="vac_status"  class="bg-light">
@@ -125,20 +70,27 @@
                         <option value="unvaccinated">Unvaccinated</option>
                     </select> 
                 </div>
-                <div class="col-md-6"> <label for="vaccine">Status</label><div class="ml-auto"> 
-                    <button class="btn button border"><b>Upload</b></button>
-                    <button class="ver btn success">Verified <i class="fa-solid fa-check-circle "></i></button>
+
                 </div>
             </div>
-            <div class="d-sm-flex align-items-center pt-5 " id="change">
-                <div class="title-info mx-3"> <b>CHANGE PASSWORD</b>
+            <div class="d-sm-flex align-items-center py-4" id="change">
+                <div class="title-info"> VERIFICATION STATUS
+                    <p>Enter verification code sent to email.</p>   
+                </div>
+                    <div class="col-md-6 d-flex justify-content-evenly" >
+                        <input name="activate" type="text" class="bg-light form-control" placeholder="Enter code..." style="width:150px"> 
+                        <button class="ver btn success">Verified <i class="fa-solid fa-check-circle "></i></button>    
+                    </div>
+                
+            </div>
+            <div class="d-sm-flex align-items-center " id="change">
+                <div class="title-info"> CHANGE PASSWORD
                     <p>Do not share your password to anyone.</p>   
                 </div>
-                <div class="ml-auto mx-3" > <button class="btn danger">Change</button> </div>
+                <div class="" > <button class="btn danger">Change</button> </div>
             </div>
-            <br>
-            <br>
-            <div class="py-2 pb-4 mx-3 "> <button class="btn btn-primary mr-3">Save Changes</button> <button class="btn border button">Cancel</button> </div>
+
+            <div class="py-2 pb-4 "> <button class="btn btn-primary mr-3">Save Changes</button> <button class="btn border button">Cancel</button> </div>
         </div>
     </div>
    
@@ -147,14 +99,8 @@
 </form>
 
 
-
-
-
-    <!-- NAavbar JS -->
-
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="assets/js/navbar.js"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 
@@ -202,7 +148,7 @@
                      success:function(data)  
                      {  
                         swal({
-                            title: "You Updated Your Account",
+                            title: "Account Updated Successfully",
                             text: "",
                             icon: "success",
                             button: "Continue",
